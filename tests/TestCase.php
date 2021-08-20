fb7bfc918b2f779dfac25990358550fa0a8d6ef2
takuya <takuya.1st+nospam@gmail.com>
2021-08-20 21:24:44 +0900

    composer.json

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,10 @@
 <?php
 
-
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+require_once __DIR__.'/../vendor/autoload.php';
+
+  use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase {
 
