5f137f14b7ae94cfea7402fe99e6a9f20fc24b10
takuya <55338+takuya@users.noreply.github.com>
2024-12-03 01:01:01 +0900

    change-dd

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -1,27 +1,6 @@
 <?php
 
 
-function dd ( ...$args ) {
-  //var_dump(debug_backtrace());
-  dump( ...$args );
-  exit;
-}
-
-;
-
-function dump ( ...$args ) {
-  foreach ( func_get_args() as $e ) {
-    var_dump( $e );
-  }
-}
-
-;
-
-function ascii_letters(){
-  $ascii_letters = array_map(function($e){return chr($e);},array_merge(range(65,90),range(97,122)));
-  return $ascii_letters;
-}
-
 
 if( !function_exists('mktemp_file') ){
   
